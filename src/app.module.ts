0fd54ce528922eb5bdfe3e9d577fe5626259a32f
soroush oracle <soroushoracle@hotmail.com>
2022-03-11 13:31:37 +0330

    binance module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { BinanceModule } from './binance/binance.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        BinanceModule,
     ],
 })
 export class AppModule {}
