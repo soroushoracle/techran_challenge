ce24d2b7785d67f13f2b46f606135621b70a3f8e
soroush oracle <soroushoracle@hotmail.com>
2022-03-11 23:14:50 +0330

    binance token api interface

---
+++ src/binance/interface/binance-token-api.interface.ts
@@ -0,0 +1,11 @@
+export class BinanceTokenApi {
+    symbol: string
+    markPrice: string
+    indexPrice: string
+    estimatedSettlePrice: string
+    lastFundingRate: string
+    interestRate: string
+    nextFundingTime: Date
+    time: Date
+    price?: number
+}
