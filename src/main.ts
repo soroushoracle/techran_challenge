3837cc3a79a547ac856508afecc24d954d693951
soroush oracle <soroushoracle@hotmail.com>
2022-03-11 13:19:14 +0330

    use global exception filter pipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './common/http-exception.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     const configService = app.get(ConfigService)
 
     app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalFilters(new HttpExceptionFilter())
     await app.listen(configService.get('HTTP_PORT'))
 }
 bootstrap()
