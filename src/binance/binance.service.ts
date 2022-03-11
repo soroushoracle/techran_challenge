b4012450c581f41ec7d6da1b5f58b76713bfe53e
soroush oracle <soroushoracle@hotmail.com>
2022-03-11 23:17:27 +0330

    fetch all token and calculate rial price

--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { BinanceApi } from './binance.api'
 import { SubmitIRRDto } from './dto/submit-irr.dto'
+import { TokenListDto } from './dto/token-list.dto'
+import { BinanceTokenApi } from './interface/binance-token-api.interface'
 
 @Injectable()
 export class BinanceService {
     private IRRPrice: number
 
-    constructor(private configService: ConfigService) {
+    constructor(private configService: ConfigService, private binanceApi: BinanceApi) {
         this.IRRPrice = Number(configService.get('USDT_DEFAULT_PRICE'))
     }
 
@@ -16,5 +19,15 @@ export class BinanceService {
 
     setIRRPrice(submitIRRDto: SubmitIRRDto) {
         this.IRRPrice = submitIRRDto.price
+    }
+
+    async fetchAllTokenToIRR(filter: TokenListDto): Promise<BinanceTokenApi[]> {
+        const { name } = filter
+        const tokens: BinanceTokenApi[] = await this.binanceApi.fetchAllToken()
+        return this.calculateRialPrice(tokens).filter((token) => token.symbol.toLowerCase().includes(name))
+    }
+
+    private calculateRialPrice(tokens: BinanceTokenApi[]) {
+        return tokens.map((token) => ({ ...token, price: Math.ceil(this.IRRPrice * Number(token.markPrice)) }))
     }
 }
