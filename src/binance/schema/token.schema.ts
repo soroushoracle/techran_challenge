e4b5ece8cb292ed3f7e190488a4081caa114778d
soroush oracle <soroushoracle@hotmail.com>
2022-03-12 23:44:39 +0330

    add paginate plugin to token schema

--- src/binance/schema/token.schema.ts
+++ src/binance/schema/token.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import { Document, PaginateModel, model } from 'mongoose'
+import * as mongoosePaginate from 'mongoose-paginate'
 
 export type TokenDocument = Token & Document
 
@@ -34,3 +35,6 @@ export class Token {
 }
 
 export const TokenSchema = SchemaFactory.createForClass(Token)
+
+TokenSchema.plugin(mongoosePaginate)
+export interface TokenModel<T extends Document> extends PaginateModel<T> {}
