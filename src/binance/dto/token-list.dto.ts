6b10d27994a6d49923363e11e9b0501259eac47c
soroush oracle <soroushoracle@hotmail.com>
2022-03-13 00:31:13 +0330

    add page and limit to TokenListDto

--- src/binance/dto/token-list.dto.ts
+++ src/binance/dto/token-list.dto.ts
@@ -1,6 +1,14 @@
-import { IsOptional } from 'class-validator'
+import { IsNumber, IsNumberString, IsOptional } from 'class-validator'
 
 export class TokenListDto {
     @IsOptional()
     name?: string
+
+    @IsOptional()
+    @IsNumberString(null, { message: 'صفحه باید از نوع عدد باشد' })
+    page: number = 1
+
+    @IsOptional()
+    @IsNumberString(null, { message: 'محدودت باید از نوع عدد باشد' })
+    limit: number = 10
 }
