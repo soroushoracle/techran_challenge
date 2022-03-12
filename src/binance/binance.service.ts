7d4a4313e565d19568f1c03e9c21b01212b4c57a
soroush oracle <soroushoracle@hotmail.com>
2022-03-12 23:43:02 +0330

    create or update tokens

--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import { Model } from 'mongoose'
 import { ConfigService } from '@nestjs/config'
+import { InjectModel } from '@nestjs/mongoose'
 import { BinanceApi } from './binance.api'
 import { SubmitIRRDto } from './dto/submit-irr.dto'
 import { TokenListDto } from './dto/token-list.dto'
 import { BinanceTokenApi } from './interface/binance-token-api.interface'
+import { Token, TokenDocument } from './schema/token.schema'
 
 @Injectable()
 export class BinanceService {
     private IRRPrice: number
 
-    constructor(private configService: ConfigService, private binanceApi: BinanceApi) {
+    constructor(
+        private configService: ConfigService,
+        private binanceApi: BinanceApi,
+        @InjectModel(Token.name) private tokenModel: Model<TokenDocument>,
+    ) {
         this.IRRPrice = Number(configService.get('USDT_DEFAULT_PRICE'))
     }
 
@@ -22,12 +29,35 @@ export class BinanceService {
     }
 
     async fetchAllTokenToIRR(filter: TokenListDto): Promise<BinanceTokenApi[]> {
-        const { name } = filter
-        const tokens: BinanceTokenApi[] = await this.binanceApi.fetchAllToken()
-        return this.calculateRialPrice(tokens).filter((token) => token.symbol.toLowerCase().includes(name))
+        return Promise.all(
+            (await this.binanceApi.fetchAllToken()).map((token) =>
+                this.insertOrUpdateToken(this.calculateRialPrice(token)),
+            ),
+        )
     }
 
-    private calculateRialPrice(tokens: BinanceTokenApi[]) {
-        return tokens.map((token) => ({ ...token, price: Math.ceil(this.IRRPrice * Number(token.markPrice)) }))
+    private calculateRialPrice(token: BinanceTokenApi) {
+        return { ...token, price: Math.ceil(this.IRRPrice * Number(token.markPrice)) }
+    }
+
+    async insertOrUpdateToken(token: BinanceTokenApi): Promise<Token> {
+        return this.tokenModel.findOneAndUpdate(
+            { symbol: token.symbol },
+            {
+                symbol: token.symbol,
+                markPrice: token.markPrice,
+                indexPrice: token.indexPrice,
+                estimatedSettlePrice: token.estimatedSettlePrice,
+                lastFundingRate: token.lastFundingRate,
+                interestRate: token.interestRate,
+                nextFundingTime: token.nextFundingTime,
+                time: token.time,
+                price: token.price,
+            },
+            {
+                upsert: true,
+                new: true,
+            },
+        )
     }
 }
