4b32d00e320b06bdc555f031209dee51de509c63
soroush oracle <soroushoracle@hotmail.com>
2022-03-13 02:26:19 +0330

    persian message for submit-irr.dto

--- src/binance/dto/submit-irr.dto.ts
+++ src/binance/dto/submit-irr.dto.ts
@@ -1,9 +1,9 @@
 import { IsNotEmpty, IsNumber, Max, Min } from 'class-validator'
 
 export class SubmitIRRDto {
-    @IsNotEmpty()
-    @IsNumber()
-    @Min(0)
-    @Max(1000000)
+    @IsNotEmpty({ message: 'قیمت ضروری می‌باشد' })
+    @IsNumber({}, { message: 'قیمت می‌بایست از نوع عدد باشد' })
+    @Min(100, { message: 'حداقل مقدار قیمت می‌بایست ۱۰۰ باشد' })
+    @Max(1000000, { message: 'حداکثر مقدار قیمت می‌بایست ۱۰۰۰۰۰۰ باشد' })
     price: number
 }
